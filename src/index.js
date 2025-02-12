6039ef3f0bd8da4765829bd97a453ae21cdbbf18
julia maday <juliamady96@gmail.com>
2025-02-12 16:13:35 -0600

    prueba2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import app from './app.js';
 import database from './database.js';
 
 app.listen(process.env.PORT, () =>{
-    console.log("Server on Port: "+ process.env.PORT)
+    console.log("Servicio en el puerto: "+ process.env.PORT)
 })
 
 /* prueba de repositorio*/
